05b89ad87be6000f1bcd025d7b27a2545ba1ad0f
Offor <offorb23@gmail.com>
2021-03-12 14:45:25 +0400

    Filter Array

--- products.js
+++ products.js
@@ -2,15 +2,13 @@ var webstore = new Vue({
     el: "#App",
     data: {
         sitename: "Vue",
-        ascending: true,
-        sortBy: 'alphabetically',
-        sortP: null,
+        show: true,
         products: [
             {
                 id: 001,
                 subject: "Maths",
                 location: "London",
-                price: "$100",
+                price: 100,
                 spaces: 5,
                 image: "maths.jpg"
             },
@@ -18,15 +16,15 @@ var webstore = new Vue({
                 id: 002,
                 subject: "English",
                 location: "Manchester",
-                price: "$80",
+                price: 80,
                 spaces: 5,
                 image: "maths.jpg"
             },
             {
                 id: 003,
                 subject: "Physics",
-                location: "Sunderland",
-                price: "$40",
+                location: "Egbeda",
+                price: 40,
                 spaces: 5,
                 image: "maths.jpg"
             },
@@ -34,15 +32,15 @@ var webstore = new Vue({
                 id: 004,
                 subject: "Social",
                 location: "Sunderland",
-                price: "$40",
+                price: 40,
                 spaces: 5,
                 image: "maths.jpg"
             },
             {
                 id: 005,
                 subject: "Civic",
-                location: "Sunderland",
-                price: "$40",
+                location: "Tunderland",
+                price: 40,
                 spaces: 5,
                 image: "maths.jpg"
             },
@@ -50,7 +48,7 @@ var webstore = new Vue({
                 id: 006,
                 subject: "Gov",
                 location: "London",
-                price: "$100",
+                price: 100,
                 spaces: 5,
                 image: "maths.jpg"
             },
@@ -58,7 +56,7 @@ var webstore = new Vue({
                 id: 007,
                 subject: "Literature",
                 location: "Manchester",
-                price: "$80",
+                price: 80,
                 spaces: 5,
                 image: "maths.jpg"
             },
@@ -66,12 +64,16 @@ var webstore = new Vue({
         ],
         cart: [],
         showProduct: true,
-        filterText: '',
+        SelectedCategory: 'All',
+        ArrangeOrder: true,
+        status: false
+        
     },
 
     methods: {
         addToCart(product) {
-            this.cart.push(product.id)
+            this.cart.push(product.id);
+
         },
 
         showCheckout() {
@@ -81,6 +83,7 @@ var webstore = new Vue({
         canAddToCart(product) {
             return product.spaces > this.cartCount(product.id);
         },
+
 
         cartCount(id) {
             let count = 0;
@@ -93,12 +96,33 @@ var webstore = new Vue({
             return count; 
         },
 
+        reverseItems() {
+            return this.products.slice().reverse();
+        }
+
+        //  changeSort() {
+        //     let subjectsArray = this.products.slice(0);
+        //     function compare(a, b) {
+        //         if (a.price > b.price) 
+        //             return 1;
+        //         if (a.price < b.price)
+        //         return -1;
+        //         return 0;
+               
+        //     }
+        //     return subjectsArray.sort(compare);
+        // }
         
     },
 
     computed: {
         cartItemCount: function() {
             return this.cart.length || '';
+        },
+
+        isDisabled: function() {
+            return !this.status;
+
         },
 
         sortedProducts() {
@@ -108,23 +132,85 @@ var webstore = new Vue({
                     return 1;
                 if (a.price < b.price)
                 return -1;
-                return 0;
-               
+                return 0;     
             }
             return productsArray.sort(compare);
         },
 
-        sortedSubject() {
-            let productsArray = this.products.slice(0);
-            function compare(a, b) {
-                if (a.subject > b.subject) 
-                    return 1;
-                if (a.subject < b.subject)
-                return -1;
-                return 0;
-               
-            }
-            return productsArray.sort(compare);
-        }
+        filteredData: function() {
+            let vm = this;
+            let category = vm.SelectedCategory;
+            if(category === "All") {
+                return vm.products;
+            } else {
+                let subjectsArray = this.products.slice(0);
+                if(category === "subject") {
+                    console.log('getting here!')
+                    function compare(a, b) {
+                        if (a.subject > b.subject)
+                            return 1;
+                        if (a.subject < b.subject)
+                            return -1;
+                        return 0;
+                    }
+                    return subjectsArray.sort(compare);
+                } else {
+                    let priceArray = this.products.slice(0);
+                    if(category === "price") {
+                        function compare(pa, pb) {
+                            if (pa.price > pb.price)
+                                return 1;
+                            if (pa.price < pb.price)
+                                return -1;
+                            return 0;
+                        }
+                        return priceArray.sort(compare);
+                    } else {
+                        let locationArray = this.products.slice(0);
+                        if(category === "location") {
+                            function compare(la,lb) {
+                                if (la.location > lb.location)
+                                    return 1;
+                                if (la.location < lb.location)
+                                    return -1;
+                                return 0;
+                            }
+                            return locationArray.sort(compare);
+                        } else {
+                            let spaceArray = this.products.slice(0);
+                            if(category === "space") {
+                                function compare(sa, sb) {
+                                    if (sa.spaces > sb.spaces) {
+                                        return 1;
+                                    }
+                                    if (sa.spaces < sb.spaces) {
+                                        return -1
+                                    }
+                                    return 0;
+                                }
+                                return spaceArray.sort(compare)
+                            }
+                            
+                        } 
+                    }
+                }
+                
+             }       
+         },
+
+         filteredPeoples: function() {
+            var vm = this;
+             var category = vm.SelectedCategory;
+             if(category === "All") {
+                 return vm.products;
+            } else {
+                return vm.products.filter(function(type) {
+                     return type.category === category;
+                 });
+             }       
+         },
+
+        
+        
     }   
 });
